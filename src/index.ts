aa4477b4f71ba63f910876dd7f2ff69d7ca49ee8
Peace Thabiwa <peacethabibinflow@proton.me>
2025-11-16 23:54:22 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Re-export core types and helpers for convenience.
+
+export * from "./events/eventSchema";
+export * from "./events/block";
+export * from "./lawT/timeline";
+export * from "./lawT/timeLabeler";
+export * from "./utils/id";
